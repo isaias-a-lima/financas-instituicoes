c646c0acb033c9f7e449480536d5db2133110c59
isaias-a-lima <isaiaschaid@gmail.com>
2023-01-03 00:57:32 -0300

    Conclusão da Classe Instituicao

--- app/model/Entities/Instituicao.php
+++ app/model/Entities/Instituicao.php
@@ -7,4 +7,18 @@ class Instituicao {
     private string $email;
     private DateTime $dataCadastro;
     private int $idUsuarioResp;
+
+    public function getIdInstituicao() { return $this->idInstituicao; }
+    public function getCnpj() { return $this->cnpj; }
+    public function getNome() { return $this->nome; }
+    public function getEmail() { return $this->email; }
+    public function getDataCadastro() { return $this->dataCadastro; }
+    public function getIdUsuarioResp() { return $this->idUsuarioResp; }
+    
+    public function setIdInstituicao(int $idInstituicao) { $this->idInstituicao = $idInstituicao; }
+    public function setCnpj(int $cnpj) { $this->cnpj = $cnpj; }
+    public function setNome(int $nome) { $this->nome = $nome; }
+    public function setEmail(int $email) { $this->email = $email; }
+    public function setDataCadastro(int $dataCadastro) { $this->dataCadastro = $dataCadastro; }
+    public function setIdUsuarioResp(int $idUsuarioResp) { $this->idUsuarioResp = $idUsuarioResp; }
 }
