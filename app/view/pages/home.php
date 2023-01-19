1937db10bea4b6556daa76329fed2ce14454ba70
isaias-a-lima <isaiaschaid@gmail.com>
2023-01-19 01:38:17 -0300

    Altera ícones das telas home e instituições

--- app/view/pages/home.php
+++ app/view/pages/home.php
@@ -1,31 +1,28 @@
 <?php
 
-use app\controller\RenderController;
 use app\controller\SessionController;
 
 $usuario = SessionController::getSessionUser();
 
-$exit = "<a href='./?p=0'>Sair.</a>";
-$welcome = "<p>Bem vindo(a) " . $usuario->getNome() . " - " . $exit . "</p>";
+$exit = "<a href='./?p=0' title='Sair' alt='Sair' class='icones'><span class='glyphicon glyphicon-log-out'></span></a>";
+$edit = "<a href='' title='Editar' alt='Editar' class='icones'><span class='glyphicon glyphicon-edit'></span></a>";
+$welcome = "<p>Bem vindo(a) " . $usuario->getNome() . $edit . $exit . "</p>";
 
 $msg = isset($_GET['msg']) ? $_GET['msg'] : null;
 $error = isset($msg) ? "<div class='alert alert-success'>$msg</div>" : "";
 
 ?>
-<section class="row">
-    <section class="col-sm-12">
-        <?=$welcome?>
-        <nav class="btn-group">
-            <a href="./?p=<?=RenderController::PAGES['CADASTRO_INSTITUICAO']['cod']?>" class="btn btn-primary">Cadastrar instituição</a>
-            <a href="#" class="btn btn-primary">Editar meus dados</a>
-        </nav>
-    </section>
-</section>
+
 <section class="row">
     <div class="col-sm-12">
-
-        <?=$error?>
-
-        <?php include "instituicoes.php"; ?>          
+        <?=$welcome?>        
     </div>
 </section>
+
+<section class="row">
+    <div class="col-sm-12">
+        <?=$error?>                  
+    </div>
+</section>
+
+<?php include "instituicoes.php"; ?>
--- app/view/pages/instituicoes.php
+++ app/view/pages/instituicoes.php
@@ -2,10 +2,26 @@
 
 use app\controller\InstituicaoController;
 use app\controller\SessionController;
+use app\controller\RenderController;
 
 $usuario = isset($usuario) ? $usuario : SessionController::getSessionUser();
 
 $instituicaoController = new InstituicaoController();
 
-echo $instituicaoController->renderizeAllInstituicoes($usuario->getIdUsuario());
 ?>
+<section class="row">
+    <div class="col-md-2">
+        <h2>Instituições</h2>
+    </div>
+    <div class="col-md-10">
+        <a href="./?p=<?= RenderController::PAGES['CADASTRO_INSTITUICAO']['cod'] ?>" title="Incluir" class="icones">
+            <span class="glyphicon glyphicon-plus"></span>
+        </a>
+    </div>
+</section>
+
+<section class="row">
+    <div class="col-md-12">
+        <?php echo $instituicaoController->renderizeAllInstituicoes($usuario->getIdUsuario()); ?>
+    </div>
+</section>
