d4f4b65762ce54a4fd41a77f92d49b8c9bae59e3
Isaias Lima <isaiaschaid@gmail.com>
2023-07-04 13:25:05 +0100

    Ajuste EmailUtil

--- app/lib/EmailUtil.php
+++ app/lib/EmailUtil.php
@@ -13,7 +13,7 @@ class EmailUtil
         $this->message = $message;
         $this->header = "MIME-Version: 1.0" . "\r\n";        
         $this->header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $this->header .= 'From: <suporte@ikdesigns.com>' . "\r\n";        
+        $this->header .= 'From: <suporte@ikdesigns.com.br>' . "\r\n";        
     }
 
     public function sendMail() {
