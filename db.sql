736c97f5c9e426585b2536fc1792650cb53886e0
Isaias Lima <isaiaschaid@gmail.com>
2023-07-16 02:19:10 +0100

    Atualiza EntitiesModule e DB

--- db.sql
+++ db.sql
@@ -1,18 +1,19 @@
-CREATE DATABASE `financas_instituicoes` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+/*CREATE DATABASE `financas_instituicoes` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;*/
 
-use financas_instituicoes;
+/*use financas_instituicoes;*/
 
 CREATE TABLE `usuarios` (
   `idusuario` int NOT NULL AUTO_INCREMENT,
-  `rg` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `nome` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `rg` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
+  `nome` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
   `datacadastro` date NOT NULL,
   PRIMARY KEY (`idusuario`),
   UNIQUE KEY `idusuario_UNIQUE` (`idusuario`),
   UNIQUE KEY `rg_UNIQUE` (`rg`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
 
 CREATE TABLE `usersecurity1` (
   `idusuario` int NOT NULL,
@@ -23,24 +24,49 @@ CREATE TABLE `usersecurity1` (
 
 CREATE TABLE `instituicoes` (
   `idinstituicao` int NOT NULL AUTO_INCREMENT,
-  `cnpj` varchar(20) COLLATE utf8_bin NOT NULL,
-  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
-  `emailcontab` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `cnpj` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
+  `nome` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_bin NOT NULL,
+  `emailcontab` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_bin DEFAULT NULL,
   `datacadastro` date NOT NULL,
   `idusuarioresp` int NOT NULL,
   PRIMARY KEY (`idinstituicao`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `cnpj_UNIQUE` (`cnpj`),
-  KEY `fk_usuarios_instituicoes_idx` (`idusuarioresp`),
-  CONSTRAINT `fk_usuarios_instituicoes` FOREIGN KEY (`idusuarioresp`) REFERENCES `usuarios` (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `fk_usuarios_instituicoes_idx` (`idusuarioresp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
 
 CREATE TABLE `usuarios_instituicoes` (
   `idusuario` int NOT NULL,
-  `idinstituicao` int NOT NULL,
+  `idinstituicao` bigint NOT NULL,
+  `funcao` varchar(20) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idusuario`,`idinstituicao`),
   KEY `fk_instituicoes_join_idx` (`idinstituicao`),
-  CONSTRAINT `fk_instituicoes_join` FOREIGN KEY (`idinstituicao`) REFERENCES `instituicoes` (`idinstituicao`),
   CONSTRAINT `fk_usuarios_join` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+
+CREATE TABLE `categorias` (
+  `idcategoria` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `tipo` char(1) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+CREATE TABLE `entradas` (
+  `identrada` int NOT NULL AUTO_INCREMENT,
+  `idinstituicao` int NOT NULL,
+  `idusuario` int NOT NULL,
+  `idcategoria` int DEFAULT NULL,
+  `dataentrada` date NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `valor` decimal(7,2) NOT NULL,
+  PRIMARY KEY (`identrada`),
+  KEY `fk_instituicoes_entradas_idx` (`idinstituicao`),
+  KEY `fk_usuarios_entradas_idx` (`idusuario`),
+  KEY `fk_categorias_entradas_idx` (`idcategoria`),
+  CONSTRAINT `fk_categorias_entradas` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`idcategoria`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_instituicoes_entradas` FOREIGN KEY (`idinstituicao`) REFERENCES `instituicoes` (`idinstituicao`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_usuarios_entradas` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
