64d4433c67cb70707d9bdd4840ba20496ddda47f
Isaias Lima <isaiaschaid@gmail.com>
2023-08-27 00:33:07 +0100

    Cria método hasFechamento()

--- app/controller/FechamentoController.php
+++ app/controller/FechamentoController.php
@@ -1,7 +1,29 @@
 <?php
 namespace app\controller;
 
+use app\lib\Validacoes;
+use app\model\dao\FechamentoDAO;
+use Exception;
+
 class FechamentoController {
+
+    private FechamentoDAO $fechamentoDAO;
+
+    public function __construct() {
+        $this->fechamentoDAO = new FechamentoDAO();
+    }
+
+    public function hasFechamento(int $idInstituicao,string $dataAtual):bool {
+        $result = false;
+        try {
+            Validacoes::validParam($idInstituicao, "ID Instituição");
+            Validacoes:: validParam($dataAtual, "Data atual");
+            $result = $this->fechamentoDAO->hasFechamento($idInstituicao, $dataAtual);
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return $result;
+    }
     
 }
 ?>
--- app/model/dao/FechamentoDAO.php
+++ app/model/dao/FechamentoDAO.php
@@ -2,8 +2,38 @@
 namespace app\model\dao;
 
 use app\model\dao\patterns\DaoPattern;
+use app\model\dao\sql\SqlBuilder;
+use app\model\entities\converter\BooleanConverter;
+use app\model\entities\converter\FechamentoConverter;
+use Exception;
 
 class FechamentoDAO extends DaoPattern {
+
+    public function hasFechamento(int $idInstituicao,string $dataAtual):bool {
+        $sql = SqlBuilder::build()->DATABASE(parent::getDbName())->
+            SELECT()->
+            addColum("count(f.idinstituicao) > 0 as chave")->
+            FROM("fechamentos f")->
+            WHERE("f.idinstituicao = :idinstituicao")->
+            AND(":dataatual between f.datainicio and f.datafim")->
+        getSql();
+
+
+        $params = [
+            [':idinstituicao', $idInstituicao],
+            [':dataatual', $dataAtual]
+        ];
+
+        $result = false;
+
+        try {
+            $result = (bool) parent::getOne($sql, $params, new BooleanConverter());
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return $result;
+    }
 
 }
 ?>
