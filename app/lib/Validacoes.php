517488f1341cb58dfda82b719bcbb781a13d9b99
Isaias Lima <isaiaschaid@gmail.com>
2023-08-27 00:26:48 +0100

    Cria novos metodos de validação

--- app/lib/Validacoes.php
+++ app/lib/Validacoes.php
@@ -6,11 +6,29 @@ use Exception;
 class Validacoes {
     public static function validParam($param, string $paramName) {
         if (!isset($param)) {
-            if (!isset($paramName)) {
+            if (isset($paramName)) {
                 $msg = "Parâmetro " . $paramName . " inválido.";
                 throw new Exception($msg);
             }
             throw new Exception("Parâmetro inválido.");
+        }
+    }
+
+    public static function validParamAndRiseMessage($param, string $message) {
+        if (!isset($param)) {
+            if (isset($message)) {
+                throw new Exception($message);
+            }
+            throw new Exception("Parâmetro inválido.");
+        }
+    }
+
+    public static function isTrueThenRiseMessage($param, string $message) {
+        if (isset($param) && is_bool($param) && $param) {
+            if (isset($message)) {
+                throw new Exception($message);
+            }
+            throw new Exception("Condição verdadeira.");
         }
     }
 }
